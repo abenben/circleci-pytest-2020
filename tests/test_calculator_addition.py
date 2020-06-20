e765d2e85ea76761c39c36df4b434bb6c7f6f49a
abenben <abenbenben@gmail.com>
2020-06-21 05:57:20 +0900

    Update and rename test_calculator.py to test_calculator_addition.py

--- tests/test_calculator_addition.py
+++ tests/test_calculator_addition.py
@@ -25,20 +25,3 @@ def test_addition_normal(addition_task):
     cal = calculator.Calculator()
     result = cal.addition(addition_task[0], addition_task[1])
     assert result == addition_task[2]
-
-
-subtraction_lists = (
-    [0, 0, 0],
-    [1, 1, 0],
-    [10, 5, 5])
-
-
-@pytest.fixture(params=subtraction_lists, ids=func_ids)
-def subtraction_task(request):
-    return request.param
-
-
-def test_subtraction_normal(subtraction_task):
-    cal = calculator.Calculator()
-    result = cal.subtraction(subtraction_task[0], subtraction_task[1])
-    assert result == subtraction_task[2]
