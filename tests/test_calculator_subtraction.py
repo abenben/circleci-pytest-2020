9dedb66ec6ab12ef38f094fea76a8f3ea3287f2c
abenben <abenbenben@gmail.com>
2020-07-03 19:29:27 +0900

    テスト失敗させる

--- tests/test_calculator_subtraction.py
+++ tests/test_calculator_subtraction.py
@@ -15,12 +15,12 @@ subtraction_lists = (
     [10, 5, 5])
 
 
-@pytest.fixture(params=subtraction_lists, ids=func_ids)
-def subtraction_task(request):
-    return request.param
+#@pytest.fixture(params=subtraction_lists, ids=func_ids)
+#def subtraction_task(request):
+#    return request.param
 
 
-def test_subtraction_normal(subtraction_task):
-    cal = calculator.Calculator()
-    result = cal.subtraction(subtraction_task[0], subtraction_task[1])
-    assert result == subtraction_task[2]
+#def test_subtraction_normal(subtraction_task):
+#    cal = calculator.Calculator()
+#    result = cal.subtraction(subtraction_task[0], subtraction_task[1])
+#    assert result == subtraction_task[2]
