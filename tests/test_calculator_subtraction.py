a39cb090bb2032a28141aa987946e13b6a66e759
abenben <abenbenben@gmail.com>
2020-07-03 21:39:07 +0900

    テストケース修正

--- tests/test_calculator_subtraction.py
+++ tests/test_calculator_subtraction.py
@@ -9,18 +9,19 @@ def func_ids(params):
         format(params[0], params[1], params[2])
 
 
-subtraction_lists = (
-    [0, 0, 0],
-    [1, 1, 0],
-    [10, 5, 5])
+subtraction_lists = ()
+#subtraction_lists = (
+#    [0, 0, 0],
+#    [1, 1, 0],
+#    [10, 5, 5])
 
 
-#@pytest.fixture(params=subtraction_lists, ids=func_ids)
-#def subtraction_task(request):
-#    return request.param
+@pytest.fixture(params=subtraction_lists, ids=func_ids)
+def subtraction_task(request):
+    return request.param
 
 
-#def test_subtraction_normal(subtraction_task):
-#    cal = calculator.Calculator()
-#    result = cal.subtraction(subtraction_task[0], subtraction_task[1])
-#    assert result == subtraction_task[2]
+def test_subtraction_normal(subtraction_task):
+    cal = calculator.Calculator()
+    result = cal.subtraction(subtraction_task[0], subtraction_task[1])
+    assert result == subtraction_task[2]
